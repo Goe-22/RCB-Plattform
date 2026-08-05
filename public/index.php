0b9cedb462220dc545168ab9247beb729f5035ec
Goe-22 <christian.goehler@freenet.de>
2026-08-05 09:13:11 +0200

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+?><!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>RC Bande Zwickau</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+<h1>Willkommen bei der RC Bande Zwickau</h1>
+
+<p>Die neue Vereinsplattform befindet sich im Aufbau.</p>
+
+</body>
+
+</html>

4c24b1b34620caf0aa1baf971c5001a2ce77eb9f
Goe-22 <christian.goehler@freenet.de>
2026-08-05 09:16:56 +0200

    Add header layout for the website

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>RC Bande Zwickau</title>
+
+<link rel="stylesheet" href="/assets/css/style.css">
+
+</head>
+
+<body>
+
+<header class="header">
+
+<div class="logo">
+
+<a href="/">
+
+RC Bande Zwickau
+
+</a>
+
+</div>
+
+<nav>
+
+<ul>
+
+<li><a href="/">Home</a></li>
+
+<li><a href="/verein">Verein</a></li>
+
+<li><a href="/strecken">Strecken</a></li>
+
+<li><a href="/termine">Termine</a></li>
+
+<li><a href="/reglement">Reglement</a></li>
+
+<li><a href="/ergebnisse">Ergebnisse</a></li>
+
+<li><a href="/kontakt">Kontakt</a></li>
+
+</ul>
+
+</nav>
+
+</header>
+
+<main>

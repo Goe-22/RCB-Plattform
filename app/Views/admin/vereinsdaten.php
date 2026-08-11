f5422ab8bf500985e50e3179089af55e4a83465e
Goe-22 <christian.goehler@freenet.de>
2026-08-11 12:10:00 +0200

    Add Vereinsdaten form for club information

---
+++ app/Views/admin/vereinsdaten.php
@@ -0,0 +1,78 @@
+<section class="admin-header">
+
+    <h1>Vereinsdaten</h1>
+
+    <p>
+        Grundlegende Informationen über die RC Bande Zwickau e.V.
+    </p>
+
+</section>
+
+<section class="admin-form">
+
+    <form method="post">
+
+        <label for="name">
+            Vereinsname
+        </label>
+
+        <input
+            type="text"
+            id="name"
+            name="name"
+            value="RC Bande Zwickau e.V."
+        >
+
+        <label for="description">
+            Kurzbeschreibung
+        </label>
+
+        <textarea
+            id="description"
+            name="description"
+            rows="6"
+        >Gemeinsam teilen wir die Leidenschaft für den RC-Modellsport.</textarea>
+
+        <label for="email">
+            E-Mail
+        </label>
+
+        <input
+            type="email"
+            id="email"
+            name="email"
+            value=""
+        >
+
+        <label for="phone">
+            Telefon
+        </label>
+
+        <input
+            type="text"
+            id="phone"
+            name="phone"
+            value=""
+        >
+
+        <label for="facebook">
+            Facebook
+        </label>
+
+        <input
+            type="url"
+            id="facebook"
+            name="facebook"
+            value=""
+        >
+
+        <button
+            type="submit"
+            class="button"
+        >
+            Änderungen speichern
+        </button>
+
+    </form>
+
+</section>

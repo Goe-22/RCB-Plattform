9507a78c5cbb6923950b985bf91593b3d5a4a970
Goe-22 <christian.goehler@freenet.de>
2026-08-12 09:09:26 +0200

    Create backup.php

---
+++ app/Views/admin/backup.php
@@ -0,0 +1,39 @@
+<section class="admin-header">
+
+    <h1>Backup</h1>
+
+    <p>
+        Sicherungen der Webseite und Einstellungen.
+    </p>
+
+</section>
+
+<section class="admin-form">
+
+    <h2>Manuelles Backup</h2>
+
+    <p>
+        Erstellt eine Sicherung der gespeicherten
+        Webseiteninformationen.
+    </p>
+
+    <form method="post">
+
+        <button
+            type="submit"
+            class="button"
+        >
+            Backup erstellen
+        </button>
+
+    </form>
+
+    <hr>
+
+    <h2>Letzte Sicherung</h2>
+
+    <p>
+        Noch keine Sicherung vorhanden.
+    </p>
+
+</section>
